9988a5ed84fd138094c9373948eb4c9395739b8b
dkmqflx <dkmqflx@gmail.com>
2022-03-23 18:21:55 +0900

    8. Blockchain Creating a Block

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,13 @@
-class Human {
-    constructor(name, age, gender) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+class Block {
+    constructor(index, hash, previousHash, data, timestamp) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
 }
-const man = new Human('Name', 20, 'male');
-console.log('man', man);
+const genesisBlock = new Block(0, '2dsf0afasodf', '', 'hello', 1234);
+let blockchain = [genesisBlock]; // array
+console.log(blockchain);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
 
-  constructor(name: string, age: number, gender: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+  constructor(index: number, hash: string, previousHash: string, data: string, timestamp: number) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-const man = new Human('Name', 20, 'male');
-console.log('man', man);
+const genesisBlock: Block = new Block(0, '2dsf0afasodf', '', 'hello', 1234);
+
+let blockchain: [Block] = [genesisBlock]; // array
+console.log(blockchain);

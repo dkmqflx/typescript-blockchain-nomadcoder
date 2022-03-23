56f3d123f973e70ecdc7efe10943e761df922777
dkmqflx <dkmqflx@gmail.com>
2022-03-23 18:14:53 +0900

    7. Classes on TypeScript Part One

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,10 @@
-console.log('hello');
+class Human {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+const man = new Human('Name', 20, 'male');
+console.log('man', man);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('hello');
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+}
+
+const man = new Human('Name', 20, 'male');
+console.log('man', man);
